14c6a31fcfbf7bae32fb5370bae97cd8ad235fa9
FRANCISCO GOMEZ LOPEZ <franciscojgomezl@gmail.com>
2018-02-28 16:50:43 -0400

    finish all requirements to prototype

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,12 @@ Rails.application.routes.draw do
 
   resources :user_stocks, only: [:create, :destroy]
 
+  get 'my_friends', to: 'users#my_friends'
+
+  # friendships routes
+  resources :users, only: [:show]
+  resources :friendships
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
+
 end
