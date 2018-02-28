86b312a66f29a391516ba5da145dcf49640549f1
FRANCISCO GOMEZ LOPEZ <franciscojgomezl@gmail.com>
2018-02-28 14:37:42 -0400

    add views and navigation for current user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'users#my_portfolio'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'welcome', to: 'welcome#index'
 
   get 'search_stocks', to: 'stocks#search'
 
